0b652f9928bb26f2ee239e7ed81ccba0de58aaf0
Federico Marini <marinif@uni-mainz.de>
2021-01-20 22:43:01 +0100

    pkg documentation itself to simplify imports management

---
+++ R/quantiseqr-pkg.R
@@ -0,0 +1,15 @@
+#' quantiseqr package
+#'
+#' pkg description
+#'
+#'
+#' @importFrom utils read.table read.csv
+#' @importFrom MASS rlm
+#' @importFrom limSolve lsei
+#' @importFrom preprocessCore normalize.quantiles
+#' @importFrom methods is
+#' @importFrom stats aggregate median
+#'
+#' @name quantiseqr-pkg
+#' @docType package
+NULL
